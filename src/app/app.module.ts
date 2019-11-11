a2feb0c7ae34af73356a4654ed1c84a76e86511f
linhbh25796 <linhbh25796@gmail.com>
2019-11-11 14:45:06 +0700

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import {AppRoutingModule} from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    TimelinesComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
